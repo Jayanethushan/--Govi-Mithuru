0c360e4aec555ec292b776c4b0cde8cf3d2b91d2
Jayanethushan <jayanethushan514@gmail.com>
2026-02-07 16:15:44 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,25 @@
-self.addEventListener('install', (e) => {
-    e.waitUntil(
-      caches.open('govi-store').then((cache) => cache.addAll([
-        'index.html',
-        'manifest.json'
-      ]))
-    );
-  });
-  
-  self.addEventListener('fetch', (e) => {
-    e.respondWith(
-      caches.match(e.request).then((response) => response || fetch(e.request))
-    );
-  });
+const CACHE_NAME = "govi-measure-v1";
+
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icon-192.png",
+  "./icon-512.png",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.js",
+  "https://unpkg.com/leaflet@1.9.4/dist/leaflet.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.js",
+  "https://cdnjs.cloudflare.com/ajax/libs/leaflet.draw/1.0.4/leaflet.draw.css"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener("fetch", e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
